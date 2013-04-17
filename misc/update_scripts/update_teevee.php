5a2a54140a431ec8202e26e8896bd80c608786eb
Michael Writhe <michael@writhem.com>
2013-04-17 13:43:14 -0600

    removed teevee stuff... it's in the reqscrapper now anyway

--- misc/update_scripts/update_teevee.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('config.php');
-require_once(WWW_DIR.'/lib/teevee.php');
-
-$t = new TeeVee(true);
-if(isset($argv[1]))
-    $t->loadFromRemote($page = $argv[1]);
-$t->processReleases(5);
-?>
